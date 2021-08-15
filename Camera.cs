0b128b29f6e991dd1a99a4ca2d3718e4c4f5371f
IamPimdedd <hasamu.44@gmail.com>
2021-08-15 10:29:25 +0700

    Player Info and score point

---
+++ Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    private Vector3 offset;
+    public GameObject player;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rigi;
+    private Vector3 post;
+    public float speed;
+    public int point;
+    public Text score;
+    public GameObject win;
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        post = new Vector3(moveHorizontal,0,moveVertical);
+        rigi.AddForce(post*speed);
+        score.text = "Score : " + point.ToString();
+        if (point == 12)
+        {
+            win.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Collect")
+        {
+            point += 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
